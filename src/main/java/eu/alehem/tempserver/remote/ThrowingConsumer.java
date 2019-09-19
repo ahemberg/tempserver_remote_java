3f2e6459efba2eaf0def775e9db640a57c3bc001
Alexander Hemberg <alexander.hemberg@gmail.com>
2019-09-19 22:08:17 +0200

    Databasemanager test (#3)
    Add tests for DatabaseManager
    
    Cleanup Databasemanager
    Replace for with forEach
    Add overloaded methods

---
+++ src/main/java/eu/alehem/tempserver/remote/ThrowingConsumer.java
@@ -0,0 +1,7 @@
+package eu.alehem.tempserver.remote;
+
+@FunctionalInterface
+public interface ThrowingConsumer<T, E extends Exception> {
+    void accept(T t) throws E;
+}
+
---
+++ src/test/java/eu/alehem/tempserver/remote/DatabaseManagerTest.java
@@ -0,0 +1,146 @@
+package eu.alehem.tempserver.remote;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Instant;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
+@Test
+public class DatabaseManagerTest {
+
+  private final String DATABASE_PATH = "src/test/resources/test.db";
+  private final String TEST_DATABASE_URL = "jdbc:sqlite:src/test/resources/test.db";
+
+  private final Set<Temperature> REFERENCE_TEMPERATURES =
+      Stream.of(
+              new Temperature("1", 2.0, Instant.ofEpochSecond(1)),
+              new Temperature("2", 2.0, Instant.ofEpochSecond(2)),
+              new Temperature("3", 2.0, Instant.ofEpochSecond(3)),
+              new Temperature("4", 2.0, Instant.ofEpochSecond(4)),
+              new Temperature("5", 2.0, Instant.ofEpochSecond(5)),
+              new Temperature("6", 2.0, Instant.ofEpochSecond(6)),
+              new Temperature("7", 2.0, Instant.ofEpochSecond(7)),
+              new Temperature("8", 2.0, Instant.ofEpochSecond(8)),
+              new Temperature("9", 2.0, Instant.ofEpochSecond(9)),
+              new Temperature("10", 2.0, Instant.ofEpochSecond(10)))
+          .collect(Collectors.toSet());
+
+  private final String DATABASE_SCHEMA =
+      "CREATE TABLE IF NOT EXISTS saved_temperatures"
+          + "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+          + " probe TEXT NOT NULL,"
+          + " temp DOUBLE NOT NULL,"
+          + " timestamp INTEGER NOT NULL,"
+          + " unique (probe, temp, timestamp) )";
+
+  @BeforeClass
+  public void beforeClass() throws SQLException {
+    Connection c = DriverManager.getConnection(TEST_DATABASE_URL);
+    Statement stmt = c.createStatement();
+    stmt.executeUpdate(DATABASE_SCHEMA);
+    stmt.close();
+    c.close();
+  }
+
+  @AfterClass
+  public void afterClass() {
+    // Delete Database
+    new File(DATABASE_PATH).delete();
+  }
+
+  @AfterMethod
+  public void afterMethod() throws SQLException {
+    Connection c = DriverManager.getConnection(TEST_DATABASE_URL);
+    Statement stmt = c.createStatement();
+    stmt.executeUpdate("DELETE FROM saved_temperatures WHERE 1=1");
+    stmt.close();
+    c.close();
+  }
+
+  @Test
+  public void testCreateDatabaseIfDoesNotExist() throws SQLException {
+    String url = "jdbc:sqlite:src/test/resources/test2.db";
+    String path = "src/test/resources/test2.db";
+    DatabaseManager.createDataBaseIfNotExists(url);
+    boolean deleted = new File(path).delete();
+    assertTrue(deleted);
+  }
+
+  @Test
+  public void testInsertAndGetTemperatures() throws SQLException {
+    DatabaseManager.insertTemperatures(REFERENCE_TEMPERATURES, TEST_DATABASE_URL);
+    Set<Temperature> fromDatabase = DatabaseManager.getTemperatures(10, TEST_DATABASE_URL);
+    assertTrue(fromDatabase.containsAll(REFERENCE_TEMPERATURES));
+  }
+
+  @Test
+  public void testCountMeasurementsInDb() throws SQLException {
+    assertEquals(DatabaseManager.countMeasurementsInDb(TEST_DATABASE_URL), 0);
+
+    DatabaseManager.insertTemperatures(
+        Stream.of(
+                new Temperature("1", 2.0, Instant.ofEpochSecond(1)),
+                new Temperature("1", 2.0, Instant.ofEpochSecond(2)))
+            .collect(Collectors.toSet()),
+        TEST_DATABASE_URL);
+    assertEquals(DatabaseManager.countMeasurementsInDb(TEST_DATABASE_URL), 2);
+  }
+
+  @Test
+  public void testAddingIdenticalMeasurementsTwiceYieldsOneEntry() throws SQLException {
+    Set<Temperature> temperatureSet =
+        Stream.of(
+                new Temperature("1", 2.0, Instant.ofEpochSecond(1)),
+                new Temperature("1", 2.0, Instant.ofEpochSecond(1)))
+            .collect(Collectors.toSet());
+
+    DatabaseManager.insertTemperatures(temperatureSet, TEST_DATABASE_URL);
+    Set<Temperature> tempsInDb = DatabaseManager.getTemperatures(9999, TEST_DATABASE_URL);
+    assertEquals(tempsInDb.size(), 1);
+    assertEquals(DatabaseManager.countMeasurementsInDb(TEST_DATABASE_URL), 1);
+  }
+
+  @Test
+  public void testDeleteTemperature() throws SQLException {
+    Temperature toRemove = new Temperature("1", 2.0, Instant.ofEpochSecond(1));
+
+    DatabaseManager.insertTemperatures(REFERENCE_TEMPERATURES, TEST_DATABASE_URL);
+    DatabaseManager.deleteTemperature(toRemove, TEST_DATABASE_URL);
+    assertEquals(
+        DatabaseManager.countMeasurementsInDb(TEST_DATABASE_URL),
+        REFERENCE_TEMPERATURES.size() - 1);
+    assertFalse(DatabaseManager.getTemperatures(9999, TEST_DATABASE_URL).contains(toRemove));
+  }
+
+  @Test
+  public void testDeleteTemperatures() throws SQLException {
+
+    Set<Temperature> toRemove =
+        Stream.of(
+                new Temperature("1", 2.0, Instant.ofEpochSecond(1)),
+                new Temperature("2", 2.0, Instant.ofEpochSecond(2)),
+                new Temperature("3", 2.0, Instant.ofEpochSecond(3)))
+            .collect(Collectors.toSet());
+
+    DatabaseManager.insertTemperatures(REFERENCE_TEMPERATURES, TEST_DATABASE_URL);
+    DatabaseManager.deleteTemperatures(toRemove, TEST_DATABASE_URL);
+    assertEquals(
+        DatabaseManager.countMeasurementsInDb(TEST_DATABASE_URL),
+        REFERENCE_TEMPERATURES.size() - 3);
+    assertFalse(DatabaseManager.getTemperatures(9999, TEST_DATABASE_URL).containsAll(toRemove));
+  }
+}
